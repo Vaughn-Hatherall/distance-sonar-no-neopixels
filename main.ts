856ef6ee854d249a0fec205d1dd48c5cdbeb3640
Vaughn-Hatherall <71399950+Vaughn-Hatherall@users.noreply.github.com>
2020-10-02 14:20:17 -0400

    final commit

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+// Created by Vaughn Hatherall
+// Created on October 1 2020
+// 
+// This program detects how far something is
+// 
 let distance2 = 0
 basic.showIcon(IconNames.Heart)
 basic.forever(function () {
